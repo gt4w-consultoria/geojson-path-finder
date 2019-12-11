19358b94308cdfe08f956555769c907c04bd98b8
João Paulo Vilela <joao.vilela@gt4w.com.br>
2019-12-11 11:51:12 -0300

    Corrigindo import do turf

--- topology.js
+++ topology.js
@@ -1,72 +1,81 @@
 'use strict';
 import roundCoord from './round-coord';
-import {explode} from 'turf';
+import { explode } from '@turf/turf';
 
-export default  topology;
+export default topology;
 
 function geoJsonReduce(geojson, fn, seed) {
-    if (geojson.type === 'FeatureCollection') {
-        return geojson.features.reduce(function reduceFeatures(a, f) {
-            return geoJsonReduce(f, fn, a);
-        }, seed);
-    } else {
-        return fn(seed, geojson);
-    }
+  if (geojson.type === 'FeatureCollection') {
+    return geojson.features.reduce(function reduceFeatures(a, f) {
+      return geoJsonReduce(f, fn, a);
+    }, seed);
+  } else {
+    return fn(seed, geojson);
+  }
 }
 
 function geoJsonFilterFeatures(geojson, fn) {
-    var features = [];
-    if (geojson.type === 'FeatureCollection') {
-        features = features.concat(geojson.features.filter(fn));
-    }
+  var features = [];
+  if (geojson.type === 'FeatureCollection') {
+    features = features.concat(geojson.features.filter(fn));
+  }
 
-    return {
-        type: 'FeatureCollection',
-        features: features
-    };
+  return {
+    type: 'FeatureCollection',
+    features: features
+  };
 }
 
 function isLineString(f) {
-    return f.geometry.type === 'LineString';
+  return f.geometry.type === 'LineString';
 }
 
 function topology(geojson, options) {
-    options = options || {};
-    var keyFn = options.keyFn || function defaultKeyFn(c) {
-            return c.join(',');
-        },
-        precision = options.precision || 1e-5;
+  options = options || {};
+  var keyFn =
+      options.keyFn ||
+      function defaultKeyFn(c) {
+        return c.join(',');
+      },
+    precision = options.precision || 1e-5;
 
-    var lineStrings = geoJsonFilterFeatures(geojson, isLineString);
-    var explodedLineStrings = explode(lineStrings);
-    var vertices = explodedLineStrings.features.reduce(function buildTopologyVertices(cs, f, i, fs) {
-            var rc = roundCoord(f.geometry.coordinates, precision);
-            cs[keyFn(rc)] = f.geometry.coordinates;
+  var lineStrings = geoJsonFilterFeatures(geojson, isLineString);
+  var explodedLineStrings = explode(lineStrings);
+  var vertices = explodedLineStrings.features.reduce(
+      function buildTopologyVertices(cs, f, i, fs) {
+        var rc = roundCoord(f.geometry.coordinates, precision);
+        cs[keyFn(rc)] = f.geometry.coordinates;
 
-            if (i % 1000 === 0 && options.progress) {
-                options.progress('topo:vertices', i, fs.length);
-            }
+        if (i % 1000 === 0 && options.progress) {
+          options.progress('topo:vertices', i, fs.length);
+        }
 
-            return cs;
-        }, {}),
-        edges = geoJsonReduce(lineStrings, function buildTopologyEdges(es, f, i, fs) {
-            f.geometry.coordinates.forEach(function buildLineStringEdges(c, i, cs) {
-                if (i > 0) {
-                    var k1 = keyFn(roundCoord(cs[i - 1], precision)),
-                        k2 = keyFn(roundCoord(c, precision));
-                    es.push([k1, k2, f.properties]);
-                }
-            });
+        return cs;
+      },
+      {}
+    ),
+    edges = geoJsonReduce(
+      lineStrings,
+      function buildTopologyEdges(es, f, i, fs) {
+        f.geometry.coordinates.forEach(function buildLineStringEdges(c, i, cs) {
+          if (i > 0) {
+            var k1 = keyFn(roundCoord(cs[i - 1], precision)),
+              k2 = keyFn(roundCoord(c, precision));
+            es.push([k1, k2, f.properties]);
+          }
+        });
 
-            if (i % 1000 === 0 && options.progress) {
-                options.progress('topo:edges', i, fs.length);
-            }
+        if (i % 1000 === 0 && options.progress) {
+          options.progress('topo:edges', i, fs.length);
+        }
 
-            return es;
-        }, []);
+        return es;
+      },
+      []
+    );
 
-    return {
-        vertices: vertices,
-        edges: edges
-    };
+  return {
+    vertices: vertices,
+    edges: edges
+  };
 }
